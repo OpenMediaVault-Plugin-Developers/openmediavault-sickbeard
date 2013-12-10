78f95694606ffbff46f3de8d8cc41e4de6a22cbb
Aaron Murray <aaron@omv-extras.org>
2013-12-10 07:01:40 -0600

    initial commit

---
+++ var/www/openmediavault/js/omv/module/admin/service/sickbeard/Settings.js
@@ -0,0 +1,100 @@
+/**
+ * Copyright (C) 2013 OpenMediaVault Plugin Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// require("js/omv/WorkspaceManager.js")
+// require("js/omv/workspace/form/Panel.js")
+// require("js/omv/data/Store.js")
+// require("js/omv/data/Model.js")
+// require("js/omv/form/plugin/LinkedFields.js")
+
+Ext.define("OMV.module.admin.service.sickbeard.Settings", {
+    extend : "OMV.workspace.form.Panel",
+    uses   : [
+        "OMV.data.Model",
+        "OMV.data.Store"
+    ],
+
+    initComponent : function () {
+        var me = this;
+
+        me.on('load', function () {
+            var checked = me.findField('enable').checked;
+            var showtab = me.findField('showtab').checked;
+            var parent = me.up('tabpanel');
+
+            if (!parent)
+                return;
+
+            var managementPanel = parent.down('panel[title=' + _("Web Interface") + ']');
+
+            if (managementPanel) {
+                checked ? managementPanel.enable() : managementPanel.disable();
+                showtab ? managementPanel.tab.show() : managementPanel.tab.hide();
+            }
+        });
+
+        me.callParent(arguments);
+    },
+
+    rpcService   : "Sickbeard",
+    rpcGetMethod : "getSettings",
+    rpcSetMethod : "setSettings",
+
+    getFormItems : function() {
+        var me = this;
+
+        return [{
+            xtype    : "fieldset",
+            title    : "General settings",
+            defaults : {
+                labelSeparator : ""
+            },
+            items : [{
+                xtype      : "checkbox",
+                name       : "enable",
+                fieldLabel : _("Enable"),
+                checked    : false
+            },{
+                xtype      : "checkbox",
+                name       : "showtab",
+                fieldLabel : _("Show Tab"),
+                boxLabel   : _("Show tab containing Sickbeard web interface frame."),
+                checked    : false
+            },{
+                xtype   : "button",
+                name    : "opensickbeard",
+                text    : _("Sickbeard Web Interface"),
+                scope   : this,
+                handler : function() {
+                    var link = 'http://' + location.hostname + ':8081/';
+                    window.open(link, '_blank');
+                }
+            },{
+                border: false,
+                html: "<br />"                
+            }]
+        }];
+    }
+});
+
+OMV.WorkspaceManager.registerPanel({
+    id        : "settings",
+    path      : "/service/sickbeard",
+    text      : _("Settings"),
+    position  : 10,
+    className : "OMV.module.admin.service.sickbeard.Settings"
+});
